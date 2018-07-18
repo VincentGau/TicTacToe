6e4afb57bc1dffef5dcec9fa8a40cd6b74e74d7f
Haku-Dell <g@kohaku.cc>
2018-07-18 09:34:15 +0800

    shuffle available moves

---
+++ Gobang/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gobang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue;
+            if(a < a + 1)
+            {
+                Console.WriteLine("lalala");
+            }
+            Console.WriteLine(a);
+            Console.WriteLine(a + 1);
+        }
+
+        class BoardState
+        {
+            public char[,] board { get; set; }
+            public int turn { get; set; }
+            public int depth { get; set; }
+            public int alpha { get; set; }
+            public int beta { get; set; }
+            public int winner { get; set; }
+            public BoardState nextMove { get; set; }
+
+
+            public BoardState(char[,] board, int turn, int depth, int alpha, int beta)
+            {
+                this.board = board;
+                this.turn = turn;  //0-max-computer  1-min-player
+                this.depth = depth;   // 0-root
+                this.alpha = alpha;
+                this.beta = beta;
+            }
+        }
+    }
+}
