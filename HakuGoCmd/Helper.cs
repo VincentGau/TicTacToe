27db2741e796d4f8729e6d08de44231eb2de8d72
Haku-Dell <g@kohaku.cc>
2018-07-20 17:30:24 +0800

    fix functions

---
+++ HakuGoCmd/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HakuGoCmd
+{
+    class Helper
+    {
+        // 棋盘尺寸
+        public const int ROW = 15;
+
+        public const int searchDepth = 4;
+
+        // 成五
+        public const int FIVE = 1000000;
+
+        // 活四
+        public const int FOUR = 100000;
+
+        public const int THREE = 50000;
+
+        public const int BLOCK_FOUR = 0;
+
+        public const int TWO = 0;
+
+        public static char AIMark = 'x';
+
+        public static char playerMark = 'o';
+
+    }
+}
--- HakuGoCmd/Program.cs
+++ HakuGoCmd/Program.cs
@@ -9,16 +9,17 @@ namespace HakuGoCmd
 
     class Program
     {
+        
         static void Main(string[] args)
         {
             BoardState bs = new BoardState();
             char[,] testBoard = new char[15, 15] {
-                { '_', '_', '_', '_', '_', 'o', 'o', 'o', '_', '_', 'o', 'o', 'o', 'o', '_', },// 1
+                { '_', '_', '_', '_', '_', 'o', 'o', 'o', '_', '_', 'o', 'x', 'o', 'o', '_', },// 1
                 { 'x', '_', '_', '_', '_', '_', '_', '_', 'o', '_', '_', '_', '_', '_', '_', },// 2
                 { 'x', 'x', '_', '_', '_', '_', '_', 'o', '_', '_', '_', '_', '_', '_', '_', },// 3
                 { 'x', '_', 'x', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', },// 4
                 { 'o', '_', '_', 'x', '_', 'o', '_', '_', '_', '_', '_', '_', '_', '_', '_', },// 5
-                { 'x', '_', '_', '_', 'o', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', },// 6
+                { 'x', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', },// 6
                 { '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', },// 7
                 { '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', },// 8
                 { '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', },// 9
@@ -30,15 +31,59 @@ namespace HakuGoCmd
                 { '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', },// 15
             };
 
-            List<Pos> winningPath = new List<Pos>();
-            winningPath = bs.checkFinished(testBoard);
+            // <winningPath> store the five positions if one has won;
+            // empty is the game is not finished yet;
+            // store Pos(99,99) is game is over and there is a tie;
+
+            //List<Pos> winningPath = new List<Pos>();
+            //winningPath = bs.checkFinished(testBoard);
 
             bs.board = testBoard;
-            if(bs.hasNeighbor(1, 3, 1))
-            {
-                Console.WriteLine("has neighbor.");
-            }
+            //if (bs.hasNeighbor(7, 0, 2))
+            //{
+            //    Console.WriteLine("has neighbor.");
+            //}
+            //bs.drawBoard();
+
+
             bs.drawBoard();
+            Console.WriteLine(bs.minimax());
+            bs.move();
+
+            while (true)
+            {
+                // <winningPath> store the five positions if one has won;
+                // empty is the game is not finished yet;
+                // store Pos(99,99) is game is over and there is a tie;
+                var winningPath = bs.checkFinished(bs.board);
+                // not finished
+                if (!winningPath.Any())
+                {
+                    Console.WriteLine("your turn now: ");
+                    bs.updateBoard(bs.getInputPos(), 'o');
+                    bs.drawBoard();
+                    Console.WriteLine(bs.minimax());
+                    bs.move();
+                }
+                else
+                {
+                    if(winningPath[0].x == 99)
+                    {
+                        Console.WriteLine("Tie");
+                        break;
+                    }
+                    if(bs.board[winningPath[0].x, winningPath[0].y]  == Helper.AIMark)
+                    {
+                        Console.WriteLine("You lose");
+                        break;
+                    }
+                    if (bs.board[winningPath[0].x, winningPath[0].y] == Helper.playerMark)
+                    {
+                        Console.WriteLine("You win");
+                        break;
+                    }
+                }
+            }
         }
     }
 
@@ -104,10 +149,19 @@ namespace HakuGoCmd
 
         public void drawBoard()
         {
+            Console.Write("  ");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write(string.Format("{0, 2}", i % 10));
+            }
+            Console.WriteLine();
+
             for (int i = 0; i < board.GetLength(0); i++)
             {
+                Console.Write(string.Format("{0, 2} ", i));
                 for (int j = 0; j < board.GetLength(1); j++)
                 {
+
                     Console.Write(board[i, j]);
                     Console.Write(" ");
                 }
@@ -127,27 +181,29 @@ namespace HakuGoCmd
             {
                 board[p.x, p.y] = c;
             }
+
+            depth = 0;
         }
 
         public int minimax()
         {
             List<Pos> result = checkFinished(board);
-            if(result.Count == 5)
+            if (result.Count == 5)
             {
-                if(board[result[0].x, result[0].y] == 'x')
+                if (board[result[0].x, result[0].y] == 'x')
                 {
                     Console.WriteLine("winner is x.");
-                    return 100000;
+                    return Helper.FIVE;
                 }
 
                 if (board[result[0].x, result[0].y] == 'o')
                 {
                     Console.WriteLine("winner is o.");
-                    return -100000;
+                    return -Helper.FIVE;
                 }
             }
 
-            if(result.Count == 1)
+            if (result.Count == 1)
             {
                 Console.WriteLine("Tie.");
                 return 0;
@@ -155,10 +211,15 @@ namespace HakuGoCmd
 
             List<Pos> availableMoves = getAvailableMoves();
 
+            if (depth > Helper.searchDepth || checkFinished(board).Any())
+            {
+                return evaluate(board);
+            }
 
             // Max, computer's turn 
-            if(turn == 0)
+            if (turn == 0)
             {
+                
                 int maxInChild = int.MinValue;
                 foreach (var move in availableMoves)
                 {
@@ -178,6 +239,30 @@ namespace HakuGoCmd
                 return maxInChild;
             }
 
+            //Min, player's turn
+            else if (turn == 1)
+            {
+                
+                int minInChild = int.MaxValue;
+                foreach (var move in availableMoves)
+                {
+                    BoardState childState = new BoardState(newBoard(move, 'o'), changeTurn(turn), depth + 1, alpha, beta);
+                    int childScore = childState.minimax();
+
+                    if (childScore < minInChild)
+                    {
+                        minInChild = childScore;
+                        beta = minInChild;
+                        nextMove = childState;
+                    }
+
+                    if (alpha >= beta)
+                        break;
+                }
+
+                return minInChild;
+            }
+
             return 0;
 
         }
@@ -186,7 +271,83 @@ namespace HakuGoCmd
         // 盘面评估函数
         public int evaluate(char[,] givenBoard)
         {
+            Pos up = new Pos(-1, 0);
+            Pos down = new Pos(1, 0);
+            Pos left = new Pos(0, -1);
+            Pos right = new Pos(0, 1);
+            Pos upleft = new Pos(-1, -1);
+            Pos upright = new Pos(-1, 1);
+            Pos downleft = new Pos(1, -1);
+            Pos downright = new Pos(1, 1);
 
+            // 向八个方向检查盘面
+            List<Pos> directions = new List<Pos>() { up, down, left, right, upleft, upright, downleft, downright };
+
+            // 活四
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    foreach(var direction in directions)
+                    {
+                        // 如果延伸段超过边界 则不可能再有活四 
+                        int x = i + direction.x * 5;
+                        int y = j + direction.y * 5;
+                        if(x < 0 || x > 14 || y < 0 || y > 14)
+                        {
+                            continue;
+                        }
+                        if (givenBoard[i, j] == '_' && givenBoard[i + direction.x * 5, j + direction.y * 5] == '_')
+                        {
+                            for(int range = 2; range <= 4; range++)
+                            {
+                                if (givenBoard[i + direction.x, j + direction.y] != givenBoard[i + direction.x * range, j + direction.y * range])
+                                    break;
+                                    
+                                if(range == 4)
+                                {
+                                    if(givenBoard[i + direction.x, j + direction.y] == Helper.AIMark)
+                                        return Helper.FOUR;
+                                    if (givenBoard[i + direction.x, j + direction.y] == Helper.playerMark)
+                                        return -Helper.FOUR;
+                                }
+                            }
+                            
+                        }
+                    }
+                    
+                }
+            }
+
+
+            // 活三
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    for (int j = 0; j < 15; j++)
+            //    {
+            //        foreach (var direction in directions)
+            //        {
+            //            // 如果延伸段超过边界 则不可能再有活四 
+            //            int x = i + direction.x * 5;
+            //            int y = j + direction.y * 5;
+            //            if (x < 0 || x > 14 || y < 0 || y > 14)
+            //            {
+            //                break;
+            //            }
+            //            if (givenBoard[i, j] == '_' && givenBoard[i + direction.x * 5, j + direction.y * 5] == '_')
+            //            {
+            //                for (int range = 1; range <= 4; range++)
+            //                {
+            //                    if (givenBoard[i, j] == givenBoard[i + direction.x * range, j + direction.y * range])
+            //                        return 50000;
+            //                }
+
+
+            //            }
+            //        }
+
+            //    }
+            //}
 
             return 0;
         }
@@ -218,56 +379,46 @@ namespace HakuGoCmd
                 {
                     if (board[i, j] == '_')
                     {
-                        // 离棋子近的放在候选列表的前列
-                        if(hasNeighbor(i, j, 1))
+                        
+                        if (hasNeighbor(i, j, 2))
                         {
                             result.Add(new Pos(i, j));
                         }
-                        else if(hasNeighbor(i, j, 2))
-                        {
-                            result.Add(new Pos(i, j));
-                        }
+                       
                     }
-                    
+
                 }
             }
 
             return result;
         }
 
-        public bool hasNeighbor(int i, int j, int neighborRange)
+        public bool hasNeighbor(int i , int j, int neighborRange)
         {
-            Pos up = new Pos(-1, 0);
-            Pos down = new Pos(1, 0);
-            Pos left = new Pos(0, -1);
-            Pos right = new Pos(0, 1);
-            Pos upleft = new Pos(-1, -1);
-            Pos upright = new Pos(-1, 1);
-            Pos downleft = new Pos(1, -1);
-            Pos downright = new Pos(1, 1);
+            int leftbound = (j - neighborRange) > 0 ? (j - neighborRange) : 0;
+            int rightbound = (j + neighborRange) < 15 ? (j + neighborRange) : 0;
+            int topbound = (i - neighborRange) > 0 ? (i - neighborRange) : 0;
+            int bottombound = (i + neighborRange) <15 ? (i + neighborRange) : 0;
 
-            List<Pos> directions = new List<Pos>() { up, down, left, right, upleft, upright, downleft, downright};
-
-            for(int range = 1; range <= neighborRange; range++)
+            for(int m = topbound; m <= bottombound; m++)
             {
-                foreach (var direction in directions)
+                for(int n = leftbound; n <= rightbound; n++)
                 {
-                    int x = i + direction.x * range;
-                    int y = j + direction.y * range;
-                    x = x < 0 ? 0 : x;
-                    x = x > 14 ? 14 : x;
-                    y = y < 0 ? 0 : y;
-                    y = y > 14 ? 14 : y;
-
-                    if (board[x,y] != '_')
+                    if (m == i && n == j)
+                        continue;
+                    if (board[m, n] != '_')
                     {
                         return true;
                     }
                 }
             }
-            
-            
             return false;
+        }
+
+        public void move()
+        {
+            board = nextMove.board;
+            drawBoard();
         }
 
         //  TODO
@@ -277,6 +428,8 @@ namespace HakuGoCmd
             return false;
         }
 
+
+        // return the winning path
         public List<Pos> checkFinished(char[,] givenBoard)
         {
             List<Pos> winningPath = new List<Pos>();
@@ -392,18 +545,18 @@ namespace HakuGoCmd
                 }
             }
 
-            for(int i = 0; i < 15; i++)
+            for (int i = 0; i < 15; i++)
             {
                 for (int j = 0; j < 15; j++)
                 {
-                    if(board[i, j] == '_')
+                    if (board[i, j] == '_')
                     {
-                        Console.WriteLine("Not finished.");
+                        //Console.WriteLine("Not finished.");
                         return winningPath;    //  empty list, not finished;
                     }
                 }
             }
-            
+
             winningPath.Add(new Pos(99, 99));  //  Tie
             return winningPath;
         }
